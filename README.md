b5c0a988f756a15c08f9b4d134c67b3b987709c3
YexuanXiao <x236191110@gmail.com>
2021-06-23 23:24:43 +0800

    fix some wrongs and add new function

--- README.md
+++ README.md
@@ -11,36 +11,20 @@
 5. 防止消息内容过多，进行截取前 30 个字符
 6. 鼠标静止随机 10 到 25 秒之后人物自动恢复原位，不再看向光标
 7. 适配 ES6 语法并且根据 ESLint 建议修复多处错误
-8. 设备宽度小于 1024px 时不显示看板娘（这里的像素会随着 DPI 改变而不是和显示器像素一一对应，尤其在移动端）
+8. 自己决定设备宽度小于多少时隐藏看板娘，默认为 1024（这里的像素会随着 DPI 改变而不是和显示器像素一一对应，尤其在移动端）
+9. 时间 tips 将在进入网页后 25 到 35 秒显示
+10. 在首页时不显示回到首页按钮
+11. 在顶部时不显示回到顶部按钮
+12. 添加闲时随机消息，使用方法类似 触摸 消息，只不过名字不是 touch 而是 alternative
+13. 自己决定设备宽度小于多少时隐藏看板娘，
 
-现在你可以通过配置文件自定义显示哪些按钮，例如：
-
-```js
-
-var pio = new Paul_Pio({
-    "mode": "static",
-    "hidden": false,
-    "content": {
-        "welcome": "欢迎来到保罗的小窝"
-    },
-    "night": "single.night()",
-    "model": ["static/pio/model.json"],
-    "tips": true
-});
-
-```
-
-修改为
+4, 8, 12 条功能的使用方法参考下面：
 
 ```js
 
 var pio = new Paul_Pio({
-    "mode": "static",
-    "hidden": false,
-    "content": {
-        "welcome": "欢迎来到保罗的小窝"
-    },
-    "night": "single.night()",
+    ...
+
     "button": {
         totop: true,
         home: true,
@@ -49,8 +33,19 @@ var pio = new Paul_Pio({
         night: true,
         close: true
     }
-    "model": ["static/pio/model.json"],
-    "tips": true
+
+    ...
+
+    "content": {
+        "welcome": ["欢迎来到萧叶轩的网站", "做好心理准备了吧？"],
+        "alternative": ["呀······！你在摸哪里啊！", "你、这······，不注意点分寸我真的要生气了哦", "等下，现在不是吐槽的时候！"],
+    },
+
+    ...
+
+    "width": 1024,
+
+    ...
 });
 
 ```
@@ -66,5 +61,6 @@ var pio = new Paul_Pio({
 
 魔改版基本兼容原版，不需要过多配置。
 
+由于本人不会 PHP，所以以 PHP 插件的形式载入时并没有新功能的开关，需要手动添加
 
 傻瓜化使用方法可以参考 [给你的博客添加一个看板娘](https://mysteriouspreserve.com/blog/2021/06/14/Site-Live2D-Wiget/)
--- static/pio.js
+++ static/pio.js
@@ -20,131 +20,160 @@ var Paul_Pio = function (prop) {
         menu: document.querySelector('.pio-container .pio-action'),
         canvas: document.getElementById('pio'),
         body: document.querySelector('.pio-container'),
-        root: document.location.protocol +'//' + document.location.hostname +'/'
+        root: document.location.origin
     }
 
     /* - 方法 */
     var modules = {
+
         // 更换模型
         idol() {
             current.idol = current.idol < prop.model.length - 1 ? current.idol++ : 0
             return current.idol
         },
+
         // 创建内容
         create(tag, prop) {
             var e = document.createElement(tag)
-            if(prop.class) e.className = prop.class
+            if (prop.class) e.className = prop.class
             return e
         },
+
         // 随机内容
         rand(arr) {
             return arr[Math.floor(Math.random() * arr.length + 1) - 1]
         },
+
         // 创建对话框方法
         render(text) {
-            if(text.constructor === Array){
+            if (text.isArray || text.constructor === Array) {
                 dialog.innerText = modules.rand(text)
             }
-            else if(text.constructor === String){
+            else if (typeof text === 'string' || text.constructor === String) {
                 dialog.innerText = text
             }
-            else{
+            else {
+                try {
+                    throw text
+                }
+                catch (text) {
+                    console.log('输入类型为：' + typeof text)
+                }
                 dialog.innerText = '输入内容出现问题了 X_X'
             }
-
             dialog.classList.add('active')
-
             clearTimeout(this.t)
             this.t = setTimeout(() => {
                 dialog.classList.remove('active')
             }, Math.round(Math.random() * 10 + 15) * 1000)
         },
+
         // 移除方法
         destroy() {
             that.initHidden()
             localStorage.setItem('posterGirl', 0)
         },
-        // 是否为移动设备
-        isMobile() {
-            return window.matchMedia('(max-width: 1024px)').matches
+
+        // 检查设备宽度
+        check() {
+            return window.matchMedia('(max-width: ' + String(prop.width || 1024) + 'px)').matches
         }
     }
+
     this.destroy = modules.destroy
 
     var elements = {
-        home: modules.create('span', {class: 'pio-home'}),
-        skin: modules.create('span', {class: 'pio-skin'}),
-        info: modules.create('span', {class: 'pio-info'}),
-        night: modules.create('span', {class: 'pio-night'}),
-        close: modules.create('span', {class: 'pio-close'}),
-        totop: modules.create('span', {class: 'pio-totop'}),
-        show: modules.create('div', {class: 'pio-show'})
+        home: modules.create('span', { class: 'pio-home' }),
+        skin: modules.create('span', { class: 'pio-skin' }),
+        info: modules.create('span', { class: 'pio-info' }),
+        night: modules.create('span', { class: 'pio-night' }),
+        close: modules.create('span', { class: 'pio-close' }),
+        totop: modules.create('span', { class: 'pio-totop' }),
+        show: modules.create('div', { class: 'pio-show' })
     }
 
-    var dialog = modules.create('div', {class: 'pio-dialog'})
+    var dialog = modules.create('div', { class: 'pio-dialog' })
     current.body.appendChild(dialog)
     current.body.appendChild(elements.show)
 
     /* - 提示操作 */
     var action = {
+
         // 欢迎
         welcome() {
-            if(document.referrer !== '' && document.referrer.indexOf(current.root) === -1){
+            if (document.referrer !== '' && document.referrer.split('/')[2] == current.root) {
                 var referrer = document.createElement('a')
                 referrer.href = document.referrer
-                modules.render (prop.content.referer ? (prop.content.referer.replace(/%t/, '“' + referrer.hostname + '”')) : ('欢迎来自 “' + referrer.hostname + '” 的朋友！'))
-            }
-            else if(prop.tips){
-                var text, date = new Date()
-                date.setHours(date.getHours() + 1)
-                var hour = date.getHours / 3
-                switch(hour){
-                    case 0:
-                        text = '已经这么晚了呀，早点休息吧，晚安~'
-                        break
-                    case 1:
-                        text = '你是夜猫子吗？这么晚还不睡觉，明天起的来嘛'
-                        break
-                    case 2:
-                        text = '早上好！现在是否元气满满呢？'
-                        break
-                    case 3:
-                        text = '上午好！不要久坐，多起来走动走动哦！'
-                        break
-                    case 4:
-                        text = '中午了，该休息了，现在是午餐时间！'
-                        break
-                    case 5:
-                        text = '午后很容易犯困呢，今天的目标完成了吗？'
-                        break
-                    case 6:
-                        text = '傍晚了！窗外夕阳的景色很美丽呢，今天是否是个好天气呢'
-                        break
-                    case 7:
-                        text = '晚上好，今天过得怎么样？'
-                        break
-                    default:
-                        try {
-                            if (!hour || hour > 7 || hour < 0) throw '发生了不可能发生的事呢！'
-                        } catch (err) {
-                            console.log(err)
-                        }
-                }
-                modules.render(text)
-            }
-            else{
-                modules.render(prop.content.welcome || '欢迎来到本站！')
+                modules.render(prop.content.referer ? (prop.content.referer.replace(/%t/, '“' + referrer.hostname + '”')) : ('欢迎来自 “' + referrer.hostname + '” 的朋友！'))
+            } else if (prop.tips) {
+                this.t = setTimeout(() => {
+                    var text, date = new Date()
+                    date.setHours(date.getHours() + 1)
+                    var range = date.getHours() / 3
+                    switch (Math.floor(range)) {
+                        case 0:
+                            text = '已经这么晚了呀，早点休息吧，晚安~'
+                            break
+                        case 1:
+                            text = '你是夜猫子吗？这么晚还不睡觉，明天起的来嘛'
+                            break
+                        case 2:
+                            text = '早上好！现在是否元气满满呢？'
+                            break
+                        case 3:
+                            text = '上午好！不要久坐，多起来走动走动哦！'
+                            break
+                        case 4:
+                            text = '中午了，该休息了，现在是午餐时间！'
+                            break
+                        case 5:
+                            text = '午后很容易犯困呢，今天的目标完成了吗？'
+                            break
+                        case 6:
+                            text = '傍晚了！窗外夕阳的景色很美丽呢，今天是否是个好天气呢'
+                            break
+                        case 7:
+                            text = '晚上好，今天过得怎么样？'
+                            break
+                        default:
+                            try {
+                                if (!range || range > 7 || range < 0) throw '发生了不可能发生的事呢！'
+                            } catch (err) {
+                                console.log(err + 'hour 为 ' + range)
+                            }
+                    }
+                    modules.render(text)
+                    clearTimeout(this.t)
+                }, Math.round(Math.random() * 25 + 10) * 1000)
+            } else {
+                modules.render(prop.content.welcome || '欢迎来到' + document.location.hostname + '!')
             }
         },
+
         // 触摸
         touch() {
             current.canvas.onclick = () => {
                 modules.render(prop.content.touch || ['你在干什么？', '再摸我就报警了！', 'HENTAI!', '不可以这样欺负我啦！'])
             }
         },
+
+        // 闲时
+        alternative() {
+            var a = 2, t
+            setInterval(() => {
+                function callback() {
+                    modules.render(prop.content.alternative || ['打起精神来！', '要不要坐下来喝杯咖啡？', '无聊的时候试试读一本书？'])
+                    clearTimeout(t)
+                }
+                callback();
+                a = a * 1.5
+                time = Math.round(Math.random() * 60) * 1000 * a;
+                t = setTimeout(callback, time);
+            }, 25 * 1000);
+        },
+
         // 右侧按钮
         buttons() {
-
             // 返回首页
             elements.home.onclick = () => {
                 location.href = current.root
@@ -152,8 +181,15 @@ var Paul_Pio = function (prop) {
             elements.home.onmouseover = () => {
                 modules.render(prop.content.home || '点击这里回到首页！')
             }
-            if(!prop.button.home){
+            if (!prop.button.home && document.location.pathname != '/') {
                 current.menu.appendChild(elements.home)
+                document.addEventListener('popstate', () => {
+                    if (document.location.pathname != '/') {
+                        current.menu.removeChild(elements.home)
+                    } else {
+                        current.menu.appendChild(elements.home)
+                    }
+                });
             }
 
             // 返回顶部
@@ -165,13 +201,20 @@ var Paul_Pio = function (prop) {
                 b = ''
                 document.documentElement.scrollTop = document.body.scrollTop = 0
                 element.style.scrollBehavior = a
-                element.body.style.scrollBehavior = b
+                document.body.style.scrollBehavior = b
             }
             elements.totop.onmouseover = () => {
                 modules.render('点击这里回到顶部！')
             }
-            if(!prop.button.totop){
+            if (!prop.button.totop) {
                 current.menu.appendChild(elements.totop)
+                document.addEventListener('scroll', () => {
+                    if (document.documentElement.scrollTop == 0) {
+                        current.menu.removeChild(current.menu.querySelector('.pio-totop'))
+                    } else {
+                        current.menu.appendChild(elements.totop)
+                    }
+                });
             }
 
             // 更换模型
@@ -180,10 +223,10 @@ var Paul_Pio = function (prop) {
                 modules.render(prop.content.skin && prop.content.skin[1] ? prop.content.skin[1] : '新衣服真漂亮~')
             }
             elements.skin.onmouseover = () => {
-                modules.render( prop.content.skin && prop.content.skin[0] ? prop.content.skin[0] : '想看看我的新衣服吗？')
+                modules.render(prop.content.skin && prop.content.skin[0] ? prop.content.skin[0] : '想看看我的新衣服吗？')
             }
-            if(!prop.button.skin){
-                if(prop.model.length > 1) current.menu.appendChild(elements.skin)
+            if (!prop.button.skin) {
+                if (prop.model.length > 1) current.menu.appendChild(elements.skin)
             }
 
             // 关于我
@@ -193,23 +236,23 @@ var Paul_Pio = function (prop) {
             elements.info.onmouseover = () => {
                 modules.render('想了解更多关于我的信息吗？')
             }
-            if(!prop.button.info){
+            if (!prop.button.info) {
                 current.menu.appendChild(elements.info)
             }
 
             // 夜间模式
-            if(prop.night){
+            if (prop.night) {
                 elements.night.onclick = () => {
-                    try{
+                    try {
                         new Function('return ' + prop.night)();
-                    }catch(err){
+                    } catch (err) {
                         console.log(prop.night + ' is not a correct function call!')
                     }
                 }
                 elements.night.onmouseover = () => {
                     modules.render('夜间点击这里可以保护眼睛呢')
                 }
-                if(!prop.button.night){
+                if (!prop.button.night) {
                     current.menu.appendChild(elements.night)
                 }
             }
@@ -221,28 +264,27 @@ var Paul_Pio = function (prop) {
             elements.close.onmouseover = () => {
                 modules.render(prop.content.close || 'QWQ 下次再见吧~')
             }
-            if(!prop.button.close){
+            if (!prop.button.close) {
                 current.menu.appendChild(elements.close)
             }
         },
         custom() {
             prop.content.custom.forEach(t => {
-                if(!t.type) t.type = 'default'
+                if (!t.type) t.type = 'default'
                 var e = document.querySelectorAll(t.selector)
-
-                if(e.length){
-                    for(var j = 0; j < e.length; j++){
-                        if(t.type === 'read'){
+                if (e.length) {
+                    for (var j = 0; j < e.length; j++) {
+                        if (t.type === 'read') {
                             e[j].onmouseover = function () {
                                 modules.render('想阅读 %t 吗？'.replace(/%t/, '“' + this.innerText.substring(0, 30) + '”'))
                             }
                         }
-                        else if(t.type === 'link'){
+                        else if (t.type === 'link') {
                             e[j].onmouseover = function () {
                                 modules.render('想了解一下 %t 吗？'.replace(/%t/, '“' + this.innerText.substring(0, 30) + '”'))
                             }
                         }
-                        else if(t.text){
+                        else if (t.text) {
                             e[j].onmouseover = () => {
                                 modules.render(t.text.substring(0, 30))
                             }
@@ -259,13 +301,17 @@ var Paul_Pio = function (prop) {
             current.body.classList.add('static')
         },
         fixed() {
-            action.touch(); action.buttons()
+            action.touch()
+            action.alternative()
+            action.buttons()
         },
         draggable() {
-            action.touch(); action.buttons()
+            action.touch()
+            action.alternative()
+            action.buttons()
 
             var body = current.body
-            body.onmousedown = function (downEvent) {
+            body.addEventListener('mousedown', function (downEvent) {
                 var location = {
                     x: downEvent.clientX - this.offsetLeft,
                     y: downEvent.clientY - this.offsetTop
@@ -275,7 +321,7 @@ var Paul_Pio = function (prop) {
                     body.classList.add('active')
                     body.classList.remove('right')
                     body.style.left = (moveEvent.clientX - location.x) + 'px'
-                    body.style.top  = (moveEvent.clientY - location.y) + 'px'
+                    body.style.top = (moveEvent.clientY - location.y) + 'px'
                     body.style.bottom = 'auto'
                 }
 
@@ -284,32 +330,33 @@ var Paul_Pio = function (prop) {
                     body.classList.remove('active')
                     document.removeEventListener('mousemove', move)
                 })
-            }
+            })
         }
     }
 
     // 运行
     this.init = onlyText => {
-        if(!(prop.hidden && modules.isMobile())){
-            if(!onlyText){
+        if (!(prop.hidden && modules.check())) {
+            if (!onlyText) {
                 action.welcome()
                 loadlive2d('pio', prop.model[0])
             }
 
-            switch (prop.mode){
+            switch (prop.mode) {
                 case 'static': begin.static(); break
-                case 'fixed':  begin.fixed(); break
+                case 'fixed': begin.fixed(); break
                 case 'draggable': begin.draggable(); break
             }
 
-            if(prop.content.custom) action.custom()
+            if (prop.content.custom) action.custom()
         }
     }
 
     // 隐藏状态
     this.initHidden = () => {
+
         // ! 清除预设好的间距
-        if(prop.mode === 'draggable'){
+        if (prop.mode === 'draggable') {
             current.body.style.top = null
             current.body.style.left = null
             current.body.style.bottom = null
@@ -330,6 +377,4 @@ var Paul_Pio = function (prop) {
 }
 
 // 请保留版权说明
-if (window.console && window.console.log) {
-    console.log('%c Pio %c https://paugram.com ','color: #fff; margin: 1em 0; padding: 5px 0; background: #673ab7;','margin: 1em 0; padding: 5px 0; background: #efefef;')
-}
+console.log('%c Pio %c https://paugram.com ', 'color: #fff; margin: 1em 0; padding: 5px 0; background: #673ab7;', 'margin: 1em 0; padding: 5px 0; background: #efefef;')
